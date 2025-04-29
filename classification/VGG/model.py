a76c29e462453d5f2c3463967e9a00825e83459a
XuandYu000 <xuzhiyu2022@gmail.com>
2025-04-29 15:05:33 +0000

    Construct the basic pipeline for classication.

---
+++ classification/VGG/model.py
@@ -0,0 +1,70 @@
+from typing import Union
+
+import torch
+import torch.nn as nn
+
+class VGG(nn.Module):
+    def __init__(
+            self, features: nn.Module, num_classes: int = 1000, init_weights: bool = False, dropout: float = 0.5
+    ) -> None:
+        super().__init__()
+        self.features = features
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            # ReLU inplace=True to save memory.
+            # inplace=True: The input tensor is modified directly,
+            # which can save memory but may lead to issues if the input is needed later.
+            nn.ReLU(True),
+            nn.Dropout(dropout),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(dropout),
+            nn.Linear(4096, num_classes),
+        )
+        if init_weights:
+            for m in self.modules():
+                if isinstance(m, nn.Conv2d):
+                    nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                    if m.bias is not None:
+                        nn.init.constant_(m.bias, 0)
+                elif isinstance(m, nn.Linear):
+                    nn.init.normal_(m.weight, 0, 0.01)
+                    nn.init.constant_(m.bias, 0)
+                elif isinstance(m, nn.BatchNorm2d):
+                    nn.init.constant_(m.weight, 1)
+                    nn.init.constant_(m.bias, 0)
+
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+def make_layer(cfg: list[Union[str, int]], batch_norm: bool = False) -> nn.Sequential:
+    layers: list[nn.Module] = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            v = int(v)
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+cfgs: dict[str, list[Union[str, int]]] = {
+    "A": [64, "M", 128, "M", 256, 256, "M", 512, 512, "M", 512, 512, "M"],
+    "B": [64, 64, "M", 128, 128, "M", 256, 256, "M", 512, 512, "M", 512, 512, "M"],
+    "D": [64, 64, "M", 128, 128, "M", 256, 256, 256, "M", 512, 512, 512, "M", 512, 512, 512, "M"],
+    "E": [64, 64, "M", 128, 128, "M", 256, 256, 256, 256, "M", 512, 512, 512, 512, "M", 512, 512, 512, 512, "M"],
+}
+
+def _vgg(cfg: str, batch_norm: bool = False, **kwargs) -> VGG:
+    model = VGG(make_layer(cfgs[cfg], batch_norm=batch_norm), **kwargs)
+    return model
---
+++ classification/VGG/train.py
@@ -0,0 +1,150 @@
+import os
+import sys
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
+from PIL import Image
+from torchvision import transforms, datasets
+import torch.optim as optim
+from tqdm import tqdm
+from torch.utils.tensorboard import SummaryWriter
+
+from model import _vgg
+
+
+# 数据增强
+data_transform = {
+    "train": transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+
+        transforms.ToTensor(),
+
+        transforms.Normalize(mean=[0.55807906, 0.45261728, 0.34557677], std=[0.23075283, 0.24137004, 0.24039967])
+    ]),
+    "val": transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.55807906, 0.45261728, 0.34557677], std=[0.23075283, 0.24137004, 0.24039967])
+    ])
+}
+
+class FoodDataSet(Dataset):
+    def __init__(self, path, tfm=None, files=None):
+        super().__init__()
+        self.path = path
+        self.files = sorted([os.path.join(path, x) for x in os.listdir(path) if x.endswith(".jpg")])
+        if files != None:
+            self.files = files
+        print(f"One {path} sample", self.files[0])
+        self.transform = tfm
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        fname = self.files[idx]
+        im = Image.open(fname)
+        im = self.transform(im)
+        # im = self.data[idx]
+        try:
+            label = int(fname.split("/")[-1].split("_")[0])
+        except:
+            label = -1  # test has no label
+        return im, label
+
+def main():
+    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+
+    # config
+    # model config
+    model_size = "B"
+    batch_norm = True
+    num_classes = 11
+    init_weights = True
+    dropout = 0.2
+
+    # training config
+    num_epochs = 300
+    batch_size = 64
+    learning_rate = 1e-3
+    weight_decay = 1e-5
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = optim.Adam
+
+    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+    writer = SummaryWriter()
+    save_pth = "./VGG_{}.pth".format(model_size)
+
+    # 加载数据集路径
+    data_root = os.path.abspath(os.path.join(os.getcwd(), "../../.."))
+    image_path = os.path.join(data_root, "datasets", "food11")
+    assert os.path.exists(image_path), "dataset path does not exist."
+
+    # 加载数据集
+    train_dataset = FoodDataSet(os.path.join(image_path, "train"), data_transform["train"])
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=nw, pin_memory=True)
+
+    val_dataset = FoodDataSet(os.path.join(image_path, "validation"), data_transform["val"])
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=nw, pin_memory=True)
+
+    train_num = len(train_dataset)
+    val_num = len(val_dataset)
+    print(f"Using {train_num} images for training, {val_num} images for validation.")
+
+    # create model
+    model = _vgg(model_size, batch_norm=batch_norm, num_classes=num_classes, init_weights=init_weights, dropout=dropout)
+    model = model.to(device)
+    optimizer = optimizer(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+
+    # training
+    best_acc = 0.0
+    train_steps = len(train_loader)
+    for epoch in range(num_epochs):
+        # train
+        model.train()
+        running_loss = 0.0
+        train_bar = tqdm(train_loader, file=sys.stdout)
+        for step, data in enumerate(train_bar):
+            imgs, labels = data
+            # 将梯度清零
+            optimizer.zero_grad()
+            outputs = model(imgs.to(device))
+            loss = loss_function(outputs, labels.to(device))
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            train_bar.desc = f"train epoch[{epoch + 1}/{num_epochs}] loss:{loss:.3f} "
+            writer.add_scalar("train_loss", loss, epoch * train_steps + step)
+            writer.add_scalar("learning_rate", learning_rate, epoch * train_steps + step)
+
+        # validate
+        model.eval()
+        acc = 0.0
+        with torch.no_grad():
+            val_bar = tqdm(val_loader, file=sys.stdout)
+            for step, val_data in enumerate(val_bar):
+                val_imgs, val_labels = val_data
+                outputs = model(val_imgs.to(device))
+                pred_y = torch.max(outputs, dim=1)[1]
+                acc += (pred_y == val_labels.to(device)).sum().item()
+
+                val_bar.desc = f"valid epoch[{epoch + 1}/{num_epochs}]"
+                writer.add_scalar("valid_acc", acc / val_num, epoch * train_steps + step)
+        val_acc = acc / val_num
+        print(f"epoch {epoch + 1} train loss: {running_loss / train_steps:.3f}, val acc: {val_acc:.3f}")
+
+        # save model
+        if val_acc > best_acc:
+            best_acc = val_acc
+            torch.save(model.state_dict(), save_pth)
+            print(f"Get best model saved to {save_pth}.")
+
+    print("Training complete.")
+
+if __name__ == '__main__':
+    main()
